9cd95cb0803b1e9aed9ad7aeb6f11bb414bb1882
Mohamed Jalloh <codingwithmbj@gmail.com>
2026-03-25 21:27:51 -0500

    fixed bugs

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getAuth, type Auth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD2a6bgpys3zxzPGjU-6uLnuP4xc1yTLK4",
-  authDomain: "biba-s-ecom.firebaseapp.com",
-  projectId: "biba-s-ecom",
-  storageBucket: "biba-s-ecom.firebasestorage.app",
-  messagingSenderId: "188303761458",
-  appId: "1:188303761458:web:19cfd5fcbeff001de31c2b",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
